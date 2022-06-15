12c8ddf7eef9b3e190536256d17dc5448bf0af1a
emretzki <emre.korkmaz2407@gmail.com>
2022-06-15 18:15:04 +0300

    Added some cubes

---
+++ Assets/Scripts/CollectibleCubes.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleCubes : MonoBehaviour
+{
+
+    bool isCollected;
+    int index;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        if (isCollected == true)
+        {
+            if (transform.parent != null)
+            {
+                transform.localPosition = new Vector3(0, -index, 0);
+            }
+
+        }
+
+    }
+
+    public bool GetIsCollected()
+    {
+        return isCollected;
+    }
+
+    public void Collect()
+    {
+        isCollected = true;
+    }
+
+    public void SetIndex(int index)
+    {
+        this.index = index;
+    }
+}
---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    GameObject MainCube;
+    int height;
+    void Start()
+    {
+        MainCube = GameObject.Find("MainCube");
+
+    }
+    void Update()
+    {
+        
+        MainCube.transform.position = new Vector3(transform.position.x, height + 1 , transform.position.z);
+        this.transform.localPosition = new Vector3(0, -height, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if(other.gameObject.tag == "Collector")
+        {
+            height += 1;
+            other.gameObject.GetComponent<CollectibleCubes>().Collect();
+            other.gameObject.GetComponent<CollectibleCubes>().SetIndex(height);
+            other.gameObject.transform.parent = MainCube.transform;
+        }     
+       
+    }
+}
