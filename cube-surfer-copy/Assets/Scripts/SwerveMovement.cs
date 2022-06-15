35565bfdf4f5625872b6c6d63db9a5d9442e843a
emretzki <emre.korkmaz2407@gmail.com>
2022-06-15 12:41:14 +0300

    First steps

---
+++ cube-surfer-copy/Assets/Scripts/SwerveMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveMovement : MonoBehaviour
+{
+    private SwerveInputSystem _swerveInputSystem;
+
+    [SerializeField] private float swerveSpeed = 0.5f;
+    [SerializeField] private float forwardSpeed;
+
+
+
+    private void Awake()
+    {
+        _swerveInputSystem = GetComponent<SwerveInputSystem>();
+    }
+
+    private void Update()
+    {
+        float swerveAmount = Time.deltaTime * swerveSpeed * _swerveInputSystem.MoveFactorX;
+        transform.Translate(swerveAmount, 0, forwardSpeed * Time.deltaTime);
+        
+    }
+
+}
